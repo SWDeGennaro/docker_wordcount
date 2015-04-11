5305aa4b96ed5f85edeec72452fb7e528e45f27c
Stephen De Gennaro <ste.degennaro@live.co.uk>
2015-04-11 16:00:04 +0100

    Change kafka consumer to write to redid

---
+++ consumer/app.py
@@ -0,0 +1,14 @@
+from kafka import KafkaConsumer, KafkaClient
+from redis import Redis
+
+redis = Redis(host="redis", port=6379)
+kafka = KafkaClient('kafka:9092')
+consumer = SimpleConsumer(kafka, "words-group", "words-topic")
+consumer.seek(0, 0) #to start reading from the beginning of the queue.
+#consumer.seek(0, 1) #to start reading from current offset.
+#consumer.seek(0, 2) #to skip all the pending messages and start reading only new 
+
+for message in consumer:
+	# message value is raw byte string -- decode if necessary!
+ 	# e.g., for unicode: `message.value.decode('utf-8')`
+	redis.incr(message.value)
