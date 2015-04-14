1e5b093b3931c5ab0aeff14c8443b2992a06d82d
Stephen De Gennaro <ste.degennaro@live.co.uk>
2015-04-14 20:00:15 +0100

    Added twitter stream to the project

--- consumer/app.py
+++ consumer/app.py
@@ -1,9 +1,11 @@
 from kafka import KafkaConsumer, KafkaClient
 from redis import Redis
+import os
 
 redis = Redis(host="redis", port=6379)
+#kafka = KafkaClient(os.environ['KAFKA_1_PORT_9092_TCP_ADDR'] + ':9092')
 kafka = KafkaClient('kafka:9092')
-consumer = SimpleConsumer(kafka, "words-group", "words-topic")
+consumer = SimpleConsumer(kafka, "word-group", "word-topic")
 consumer.seek(0, 0) #to start reading from the beginning of the queue.
 #consumer.seek(0, 1) #to start reading from current offset.
 #consumer.seek(0, 2) #to skip all the pending messages and start reading only new 
--- producer/app.py
+++ producer/app.py
@@ -1,24 +1,68 @@
-from flask import Flask, render_template, request, jsonify
+import json
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from textblob import TextBlob
 from kafka import SimpleProducer, KafkaClient
 import os
+import re
+import logging
+import logging.handlers
 
-app = Flask(__name__)
+# import twitter keys and tokens
+from config import *
+
 #kafka = KafkaClient(os.environ['KAFKA_1_PORT_9092_TCP_ADDR'] + ':9092')
 kafka = KafkaClient('kafka:9092')
 producer = SimpleProducer(kafka)
 topic = "word-topic"
 
-@app.route('/')
-def index():
-    # Render template
-    return render_template('index.html')
+class TweetStreamListener(StreamListener):
 
-@app.route('/post', methods = ['POST'])
-def post():
-    # Get the parsed contents of the form data
-    json = request.json
-    print(json)
-    producer.send_messages(topic, jsonify(json))
+    # on success
+    def on_data(self, data):
+        logger = logging.getLogger('twitter_stream')
 
-if __name__ == "__main__":
-	app.run(host="0.0.0.0", debug=True)
+        # JSON decode the tweet
+        try:
+            decoded = json.loads(data)
+        except ValueError:
+            logger.warn("Could not JSON decode Twitter response")
+            return False
+
+        # Fetch the text content of the Tweet
+        tweet = ''
+        try:
+            tweet = decoded['text']
+        except KeyError:
+            logger.warn('No key text in Twitter response')
+            return False
+
+        tweet_regex = re.compile(r"#([^\s|#]+)")
+        hashtags = re.findall(tweet_regex, tweet)
+
+        for hashtag in hashtags:
+            #write to kafka
+            print hashtag.encode('utf-8')
+            producer.send_messages(topic, hashtag)
+
+        return True
+
+    # on failure
+    def on_error(self, status):
+        print status
+
+if __name__ == '__main__':
+
+    # create instance of the tweepy tweet stream listener
+    listener = TweetStreamListener()
+
+    # set twitter keys/tokens
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    # create instance of the tweepy stream
+    stream = Stream(auth, listener)
+
+    # search twitter for keyword
+    stream.filter(track=['congress'])
